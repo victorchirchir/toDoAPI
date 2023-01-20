793c82f1252b25a79f8ab8e03fd5aeae622deff0
Victor Chirchir <vctrchirchir@gmail.com>
2023-01-20 20:56:50 +0300

    added guicorn to requirements and created build.sh

--- api/settings.py
+++ api/settings.py
@@ -120,7 +120,7 @@ REST_FRAMEWORK = {
     },
     'DEFAULT_RENDERER_CLASSES': [
         'rest_framework.renderers.JSONRenderer',
-        # 'rest_framework.renderers.BrowsableAPIRenderer',
+        # 'rest_framework.renderers.BrowsableAPIRenderer',#removes the Rest framework interface leaving only json values
     ]
 }
 
