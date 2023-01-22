820cfa4d7809cc179688001feec91c3b9762f6c4
Victor Chirchir <vctrchirchir@gmail.com>
2023-01-22 12:21:36 +0300

    changed settings file

--- api/wsgi.py
+++ api/wsgi.py
@@ -11,7 +11,7 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "api.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "api.settings.production")
 
 
 application = get_wsgi_application()
